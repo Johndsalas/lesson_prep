8b010d5b91217288e5632c2cb90c44e6e9be5067
John Salas <johndsalas@gmail.com>
2021-08-29 18:34:07 -0500

    adding w3rangle exercise review

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -2,9 +2,14 @@ from env import host, user, password
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 #***************************Get Data From SQL******************************
 def get_data_from_mysql(query, database):
+    '''Takes in a SQL query and a database name
+       Returns the results of the SQL query using the input database'''
 
     # create url string from user info and desired database
     url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
@@ -15,6 +20,7 @@ def get_data_from_mysql(query, database):
 #****************************Wrangle Telco Data*****************************
 
 def wrangle_telco():
+    ''' Acquires and cleans Telco data'''
 
     query = """ 
 
@@ -42,3 +48,57 @@ def wrangle_telco():
     df = df.drop(columns=['customer_id'])
 
     return df
+
+#***************************Graphs*****************************
+
+def histographs(df):
+    '''Get histographs of preped Telco data'''
+
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = df.columns
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1,4, plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+
+def boxplots(df):
+    '''Get boxplots of preped Telco data'''
+
+    # List of columns
+    cols = df.columns
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1,3, plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        sns.boxplot(data=df[[col]])
+
+        # Hide gridlines.
+        plt.grid(False)
+        
+        # sets proper spacing between plots
+        plt.tight_layout()
