5efad5b30f314e7d02a45ae2b196733810484269
John Salas <johndsalas@gmail.com>
2021-09-02 12:45:35 -0500

    commit new wrangle

---
+++ regression/wrangle_exercise.py
@@ -0,0 +1,113 @@
+'''Imports for wrangle exercises in regression'''
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+from env import user, password, host
+
+#**************************************************Acquire*******************************************************
+
+def acquire_zillow():
+    
+    url = f"mysql+pymysql://{user}:{password}@{host}/zillow"
+    
+    query = """
+            
+    SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    FROM properties_2017
+
+    LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+
+    WHERE propertylandusedesc IN ("Single Family Residential",                       
+                                  "Inferred Single Family Residential")"""
+
+    return pd.read_sql(query, url)
+
+#**************************************************Distributions*******************************************************
+
+def get_hist(df):
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col != 'fips']
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # fix layout
+        plt.tight_layout()
+        
+        
+def get_box(df):
+    
+    plt.figure(figsize=(8,4))
+
+    sns.boxplot(data=df.drop(columns=['fips']))
+
+    # fix layout
+    plt.tight_layout()
+        
+#**************************************************Prepare*******************************************************
+
+def prepare_zillow(df):
+    
+    # drop rows that are nulls in bedroomcnt, bathroomcnt, taxvaluedollarcnt
+    df = df.dropna(subset = ['bedroomcnt','bathroomcnt', 'taxvaluedollarcnt', 'yearbuilt'])
+    
+    # drop taxamount
+    df = df.drop(columns = 'taxamount')
+    
+    # converting column datatypes
+    df.fips = df.fips.astype(object)
+    df.yearbuilt = df.yearbuilt.astype(object)
+    df.bedroomcnt = df.bedroomcnt.astype(int)
+    
+    # renaming column names to one's I like better
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'area',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built'})
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    # impute median value 
+    imputer = SimpleImputer(strategy='median') 
+
+    imputer.fit(train[['area']])
+
+    train[['area']] = imputer.transform(train[['area']])        
+    validate[['area']] = imputer.transform(validate[['area']])  
+    test[['area']] = imputer.transform(test[['area']])          
+    
+    return train, validate, test    
+
+#**************************************************Wrangle*******************************************************
+
+def wrangle_zillow():
+    
+    train, validate, test = prepare_zillow(acquire_zillow())
+    
+    return train, validate, test
